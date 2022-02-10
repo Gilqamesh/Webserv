211fd91a2669e6081897d548bc8f002540e8ea3a
Erchegyi David <edavid@2-A-1.42heilbronn.de>
2022-02-10 20:02:55 +0100

    add views directory, need: client routing in handle_connection

--- sources/server_test.cpp
+++ sources/server_test.cpp
@@ -3,6 +3,7 @@
 int main(void)
 {
     server test_server(8080, 3);
-    test_server.cache_file("sources/index.html", "/");
+    test_server.cache_file("views/index.html", "/");
+    test_server.cache_file("views/about.html", "/about");
     test_server.server_listen();
 }
